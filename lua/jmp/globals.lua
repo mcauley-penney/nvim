55abc7e09bd9b641a7c314b8b01808132cb3b675
McAuley Penney <jacobmpenney@gmail.com>
2023-04-13 16:21:25 -0700

    refactor: stop removing default plugins
    See https://github.com/neovim/neovim/pull/21369#issuecomment-1347615173
    and https://github.com/neovim/neovim/commit/880f7d12fea31ea85d14b419ef8dcbb16dee2cf4
    
    I'm tired of looking at it and want it gone

--- lua/jmp/globals.lua
+++ lua/jmp/globals.lua
@@ -20,33 +20,6 @@ vim.g.clipboard = {
 -- filetype
 vim.g.python3_host_prog = "/usr/bin/python3"
 
--- turn off a bunch of default plugins
-for _, plug in pairs({
-	"2html_plugin",
-	"getscript",
-	"getscriptPlugin",
-	"gzip",
-	"logiPat",
-	"matchit",
-	"netrw",
-	"netrwPlugin",
-	"netrwSettings",
-	"netrwFileHandlers",
-	"remote_plugins",
-	"rrhelper",
-	"spellfile_plugin",
-	"spec",
-	"tar",
-	"tarPlugin",
-	"tutor_mode_plugin",
-	"vimball",
-	"vimballPlugin",
-	"zip",
-	"zipPlugin",
-}) do
-	vim.g["loaded_" .. plug] = 1
-end
-
 -- providers
 -- https://github.com/neovim/neovim/blob/master/runtime/doc/provider.txt
 for _, provider in ipairs({ "node", "perl", "ruby" }) do
