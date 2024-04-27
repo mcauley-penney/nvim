ceee7f03741df8581f43874152f7d34832d04603
McAuley Penney <jacobmpenney@gmail.com>
2024-04-27 08:04:24 -0700

    refactor(lsp): update keymaps
    - reorganize
    - give better descriptions
    - fix parameters for functions updated in nightly

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -57,8 +57,14 @@ M.on_attach = function(client, bufnr)
     vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
   end
 
+  map('n', "[d", vim.diagnostic.goto_prev, "Previous diagnostic")
+  map('n', "]d", vim.diagnostic.goto_next, "Next diagnostic")
+  map('n', "<leader>vd", function()
+    vim.diagnostic.open_float({ border = tools.ui.cur_border, })
+  end, "[v]iew [d]iagnostic float")
+
   if client.supports_method(methods.textDocument_formatting) then
-    map('n', "<leader>f", function() lsp.format({ timeout_ms = 2000 }) end, "Format with LSP")
+    map('n', "<leader>f", function() lsp.format({ timeout_ms = 2000 }) end, "[f]ormat with LSP")
   end
 
   -- https://github.com/neovim/neovim/commit/448907f65d6709fa234d8366053e33311a01bdb9
@@ -66,17 +72,16 @@ M.on_attach = function(client, bufnr)
   if client.supports_method(methods.textDocument_inlayHint) then
     map('n', "<leader>th", function()
       local hint = vim.lsp.inlay_hint
-      hint.enable(bufnr, not hint.is_enabled(bufnr))
+      hint.enable(not hint.is_enabled(bufnr))
     end, "[t]oggle inlay [h]ints")
   end
 
   if client.supports_method(methods.textDocument_rename) then
-    map("n", "<leader>r", lsp.rename, "Rename")
-    --  map('n', "<leader>r", require(attach .. "rename").rename, "LSP [r]ename")
+    map("n", "<leader>r", lsp.rename, "[r]ename")
   end
 
   if client.supports_method(methods.textDocument_declaration) then
-    map('n', "<leader>DE", lsp.declaration, "Go To [DE]claration")
+    map('n', "<leader>de", lsp.declaration, "go to [de]claration")
   end
 
   if client.supports_method(methods.textDocument_hover) then
@@ -84,16 +89,8 @@ M.on_attach = function(client, bufnr)
   end
 
   if client.supports_method(methods.textDocument_signatureHelp) then
-    map('i', "<C-k>", lsp.signature_help, "LSP Signature Help")
+    map('i', "<C-k>", lsp.signature_help, "LSP signature help")
   end
-
-  map('n', "[d", vim.diagnostic.goto_prev, "Previous Diagnostic")
-
-  map('n', "]d", vim.diagnostic.goto_next, "Next Diagnostic")
-
-  map('n', "<leader>vd", function()
-    vim.diagnostic.open_float({ border = tools.ui.cur_border, })
-  end, "View Diagnostic Float")
 end
 
 
