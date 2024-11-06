72eff98c420322b334c12f3d1679d3c1a5bd4558
McAuley Penney <jacobmpenney@gmail.com>
2024-11-06 16:06:08 -0500

    refactor(statuscol): improve fancy-showbreak code
    Make implementation shorter through vim functions intended for this
    purpose
    
    Thanks, u/stringTrimmer

--- lua/plugins/statuscol.lua
+++ lua/plugins/statuscol.lua
@@ -3,21 +3,11 @@ local function get_num_wraps()
   local wrapped_lines = vim.api.nvim_win_call(0, function()
     local winid = vim.api.nvim_get_current_win()
 
-    -- get the width of the buffer
-    local winwidth = vim.api.nvim_win_get_width(winid)
-    local numberwidth = vim.wo.number and vim.wo.numberwidth or 0
-    local signwidth = vim.fn.exists("*sign_define") == 1 and vim.fn.sign_getdefined() and 2 or 0
-    local foldwidth = vim.wo.foldcolumn or 0
+    local winfo = vim.fn.getwininfo(winid)[1]
+    local bufwidth = winfo["width"] - winfo["textoff"]
+    local line_length = vim.fn.strdisplaywidth(vim.fn.getline(vim.v.lnum))
 
-    -- subtract the number of empty spaces in your statuscol. I have
-    -- four extra spaces in mine, to enhance readability for me
-    local bufferwidth = winwidth - numberwidth - signwidth - foldwidth - 4
-
-    -- fetch the line and calculate its display width
-    local line = vim.fn.getline(vim.v.lnum)
-    local line_length = vim.fn.strdisplaywidth(line)
-
-    return math.floor(line_length / bufferwidth)
+    return math.floor(line_length / bufwidth)
   end)
 
   return wrapped_lines
