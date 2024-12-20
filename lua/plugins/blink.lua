90fc529e590a6ab4f09fd0db4ab9c0b512407f1f
McAuley Penney <jacobmpenney@gmail.com>
2024-12-20 14:21:26 -0700

    refactor(cmp): use blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,147 +1,161 @@
---  local function get_lsp_completion_context(completion)
---    local ok, source_name = pcall(function()
---      return vim.lsp.get_client_by_id(completion.client_id).name
---    end)
---
---    if not ok then
---      return nil
---    end
---
---    if source_name == "ts_ls" then
---      return completion.detail
---    elseif source_name == "pyright" and completion.labelDetails ~= nil then
---      return completion.labelDetails.description
---    elseif source_name == "texlab" then
---      return completion.detail
---    elseif source_name == "clangd" then
---      local doc = completion.documentation
---      if doc == nil then return end
---
---      local import_str = doc.value
---      import_str = import_str:gsub("[\n]+", "")
---
---      local str
---      str = import_str:match('<(.-)>')
---      if str then
---        return '<' .. str .. '>'
---      end
---
---      str = import_str:match('["\'](.-)["\']')
---      if str then
---        return '"' .. str .. '"'
---      end
---
---      return nil
---    end
---  end
+local function get_lsp_completion_context(completion)
+  local ok, source_name = pcall(function()
+    return vim.lsp.get_client_by_id(completion.client_id).name
+  end)
+
+  if not ok then
+    return nil
+  end
+
+  if source_name == "ts_ls" then
+    return completion.detail
+  elseif source_name == "pyright" and completion.labelDetails ~= nil then
+    return completion.labelDetails.description
+  elseif source_name == "texlab" then
+    return completion.detail
+  elseif source_name == "clangd" then
+    local doc = completion.documentation
+    if doc == nil then return end
+
+    local import_str = doc.value
+    import_str = import_str:gsub("[\n]+", "")
+
+    local str
+    str = import_str:match('<(.-)>')
+    if str then
+      return '<' .. str .. '>'
+    end
+
+    str = import_str:match('["\'](.-)["\']')
+    if str then
+      return '"' .. str .. '"'
+    end
+
+    return nil
+  end
+end
 
 return {
-  --  "saghen/blink.cmp",
-  --  event = "BufReadPre",
-  --  version = "v0.*", -- REQUIRED release tag to download pre-built binaries
+  "saghen/blink.cmp",
+  version = 'v0.*',
+  opts = {
+    sources = {
+      default = { "lsp", "path", "luasnip", "lazydev", "buffer" },
+      providers = {
+        buffer = {
+          name = "buffer",
+          max_items = 4,
+        },
+        lazydev = {
+          name = "LazyDev",
+          module = "lazydev.integrations.blink",
+          fallbacks = { "LSP" },
+        },
+        lsp = {
+          name = "LSP",
+        },
+        path = {
+          name = "Path",
+          opts = { get_cwd = vim.uv.cwd },
+        },
+        snippets = {
+          name = "Snippets",
+        },
+      },
+    },
+    keymap = { preset = 'super-tab' },
+    completion = {
+      list = {
+        selection = "auto_insert",
+        cycle = { from_top = false },
+        max_items = 50,
+      },
+      documentation = {
+        window = {
+          min_width = 15,
+          max_width = 50,
+          max_height = 15,
+          border = tools.ui.borders.thin,
+        },
+        auto_show = true,
+        auto_show_delay_ms = 250,
+      },
+      ghost_text = {
+        enabled = false,
+      },
+      menu = {
+        min_width = 42,
+        max_height = 10,
+        border = tools.ui.borders.none,
+        draw = {
+          padding = 2,
+          gap = 3,
+          columns = { { 'kind_icon', gap = 2, 'label' }, { 'label_description' } },
+          components = {
+            kind_icon = {
+              ellipsis = false,
+              text = function(ctx) return ctx.kind_icon .. ctx.icon_gap end,
+              highlight = function(ctx) return 'BlinkCmpKind' .. ctx.kind end,
+            },
+            label = {
+              width = {
+                fill = true,
+                max = 60,
+              },
+              text = function(ctx) return ctx.label .. ctx.label_detail end,
+              highlight = function(ctx)
+                local highlights = {
+                  { 0, #ctx.label, group = ctx.deprecated and 'BlinkCmpLabelDeprecated' or 'BlinkCmpLabel' },
+                }
+                if ctx.label_detail then
+                  table.insert(highlights, { #ctx.label, #ctx.label + #ctx.label_detail, group = 'BlinkCmpLabelDetail' })
+                end
 
-  --  ---@module "blink.cmp"
-  --  ---@type blink.cmp.Config
-  --  opts = {
-  --    sources = {
-  --      providers = {
-  --        { "blink.cmp.sources.lsp",      name = "LSP" },
-  --        { "blink.cmp.sources.snippets", name = "Snippets", score_offset = -1 },
-  --        {
-  --          "blink.cmp.sources.path",
-  --          name = "Path",
-  --          score_offset = 3,
-  --          opts = { get_cwd = vim.uv.cwd },
-  --        },
-  --        {
-  --          "blink.cmp.sources.buffer",
-  --          name = "Buffer",
-  --          keyword_length = 3,
-  --          fallback_for = { "LSP" }, -- PENDING https://github.com/Saghen/blink.cmp/issues/122
-  --        },
-  --      },
-  --    },
-  --    highlight = { use_nvim_cmp_as_default = true },
-  --    keymap = {
-  --      accept = "<Tab>",
-  --      select_next = { "<C-n>" },
-  --      select_prev = { "<C-p>" },
-  --      scroll_documentation_down = "<PageDown>",
-  --      scroll_documentation_up = "<PageUp>",
-  --    },
-  --    accept = {
-  --      enabled = true
-  --    },
-  --    nerd_font_variant = "mono",
-  --    windows = {
-  --      documentation = {
-  --        min_width = 15,
-  --        max_width = 50,
-  --        max_height = 15,
-  --        border = tools.ui.borders.thin,
-  --        auto_show = true,
-  --        auto_show_delay_ms = 250,
-  --      },
-  --      autocomplete = {
-  --        min_width = 20,
-  --        max_height = 10,
-  --        border = tools.ui.borders.none,
-  --        selection = "preselect",
-  --        cycle = { from_top = false },
-  --        draw = function(ctx)
-  --          -- differentiate snippets from LSPs, the user, and emmet
-  --          local icon, cmp_item, source = ctx.kind_icon, ctx.item, ctx.item.source
+                for _, idx in ipairs(ctx.label_matched_indices) do
+                  table.insert(highlights, { idx, idx + 1, group = 'BlinkCmpLabelMatch' })
+                end
 
-
-  --          local cmp_ctx
-  --          if source == "LSP" then
-  --            cmp_ctx = get_lsp_completion_context(cmp_item)
-
-  --            if cmp_ctx == nil then
-  --              cmp_ctx = ""
-  --            end
-  --          end
-
-  --          return {
-  --            { " " },
-  --            {
-  --              " " .. ctx.item.label .. " ",
-  --              fill = true,
-  --              hl_group = ctx.deprecated and "BlinkCmpLabelDeprecated" or "BlinkCmpLabel",
-  --              max_width = 45,
-  --            },
-  --            { cmp_ctx },
-  --            { " " },
-  --          }
-  --        end,
-  --      },
-  --    },
-  --    kind_icons = {
-  --      Text = "󰦨",
-  --      Method = "󰊕",
-  --      Function = "󰊕",
-  --      Constructor = "",
-  --      Field = "󰇽",
-  --      Variable = "󰂡",
-  --      Class = "⬟",
-  --      Interface = "",
-  --      Module = "",
-  --      Property = "󰜢",
-  --      Unit = "",
-  --      Value = "󰎠",
-  --      Enum = "",
-  --      Keyword = "󰌋",
-  --      Snippet = "󰒕",
-  --      Color = "󰏘",
-  --      Reference = "",
-  --      File = "󰉋",
-  --      Folder = "󰉋",
-  --      EnumMember = "",
-  --      Constant = "󰏿",
-  --      Struct = "",
-  --      Event = "",
-  --      Operator = "󰆕",
-  --      TypeParameter = "󰅲",
-  --    },
-  --  },
+                return highlights
+              end,
+            },
+            label_description = {
+              width = { fill = true },
+              text = function(ctx) return get_lsp_completion_context(ctx.item) end,
+              highlight = 'BlinkCmpLabelDescription',
+            }
+          },
+        },
+      },
+    },
+    appearance = {
+      nerd_font_variant = "mono",
+      kind_icons = {
+        Text = "󰦨 ",
+        Method = '󰆦 ',
+        Function = '󰆦 ',
+        Constructor = '󰆦 ',
+        Field = ' ',
+        Variable = ' ',
+        Class = ' ',
+        Interface = ' ',
+        Module = ' ',
+        Property = ' ',
+        Unit = ' ',
+        Value = ' ',
+        Enum = ' ',
+        Keyword = ' ',
+        Snippet = ' ',
+        Color = ' ',
+        File = ' ',
+        Reference = ' ',
+        Folder = ' ',
+        EnumMember = ' ',
+        Constant = ' ',
+        Struct = ' ',
+        Event = '',
+        Operator = ' ',
+        TypeParameter = ' ',
+      },
+    }
+  },
 }
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,281 +1,235 @@
 return {
-  'iguanacucumber/magazine.nvim',
-  name = 'nvim-cmp',
-  event = { 'CmdlineEnter', 'InsertEnter' },
-  dependencies = {
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/cmp-emoji",
-
-    {
-      "petertriho/cmp-git",
-      dependencies = "nvim-lua/plenary.nvim",
-      config = true,
-      ft = "gitcommit"
-    },
-
-    "kdheepak/cmp-latex-symbols",
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-nvim-lua",
-    "hrsh7th/cmp-path",
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
-    "onsails/lspkind.nvim",
-    "windwp/nvim-autopairs",
-  },
-  config = function()
-    local cmp = require("cmp")
-    local compare = cmp.config.compare
-    local pad_len = 1
-
-    --[[
-          Get completion context, such as namespace where item is from.
-          Depending on the LSP, this information is stored in different places.
-          The process to find them is very manual: log the payloads And see where useful information is stored.
-
-          See https://www.reddit.com/r/neovim/comments/128ndxk/comment/jen9444/?utm_source=share&utm_medium=web2x&context=3
-      ]]
-    local function get_lsp_completion_context(completion, source)
-      local ok, source_name = pcall(function()
-        return source.source.client.config.name
-      end)
-
-      if not ok then
-        return nil
-      end
-
-      if source_name == "ts_ls" then
-        return completion.detail
-      elseif source_name == "pyright" and completion.labelDetails ~= nil then
-        return completion.labelDetails.description
-      elseif source_name == "texlab" then
-        return completion.detail
-      elseif source_name == "clangd" then
-        local doc = completion.documentation
-        if doc == nil then return end
-
-        local import_str = doc.value
-
-        local i, j = string.find(import_str, "[\"<].*[\">]")
-        if i == nil then return end
-
-        return string.sub(import_str, i, j)
-      end
-
-      return nil
-    end
-
-    cmp.setup({
-      formatting = {
-        fields = { "kind", "abbr", "menu" },
-        format = function(entry, vim_item)
-          if not require("cmp.utils.api").is_cmdline_mode() then
-            local abbr_width_max = 25
-            local menu_width_max = 20
-
-            local choice = require("lspkind").cmp_format({
-              ellipsis_char = tools.ui.icons.ellipses,
-              maxwidth = abbr_width_max + menu_width_max,
-              mode = "symbol",
-            })(entry, vim_item)
-
-            choice.abbr = vim.trim(choice.abbr)
-
-            local cmp_ctx = get_lsp_completion_context(entry.completion_item, entry.source)
-            if cmp_ctx ~= nil and cmp_ctx ~= "" then
-              choice.menu = cmp_ctx
-              local menu_width = string.len(choice.menu)
-
-              if menu_width > menu_width_max then
-                choice.menu = vim.fn.strcharpart(choice.menu, 0, menu_width_max - 1)
-                choice.menu = choice.menu .. tools.ui.icons.ellipses
-              else
-                local padding = string.rep(' ', menu_width_max - menu_width)
-                choice.menu = padding .. choice.menu
-              end
-            else
-              choice.menu = ""
-              abbr_width_max = abbr_width_max + (menu_width_max / 2)
-            end
-
-            -- give padding until min/max width is met
-            -- https://github.com/hrsh7th/nvim-cmp/issues/980#issuecomment-1121773499
-            local abbr_width = string.len(choice.abbr)
-            if abbr_width < abbr_width_max then
-              local padding = string.rep(' ', abbr_width_max - abbr_width)
-              vim_item.abbr = choice.abbr .. padding
-            end
-
-            return choice
-          else
-            local abbr_width_min = 12
-            local abbr_width_max = 50
-
-            local choice = require("lspkind").cmp_format({
-              ellipsis_char = tools.ui.icons.ellipses,
-              maxwidth = abbr_width_max,
-              mode = "symbol",
-            })(entry, vim_item)
-
-            choice.abbr = vim.trim(choice.abbr)
-
-            -- give padding until min/max width is met
-            -- https://github.com/hrsh7th/nvim-cmp/issues/980#issuecomment-1121773499
-            local abbr_width = string.len(choice.abbr)
-            if abbr_width < abbr_width_min then
-              local padding = string.rep(' ', abbr_width_min - abbr_width)
-              vim_item.abbr = choice.abbr .. padding
-            end
-
-            return choice
-          end
-        end,
-      },
-      mapping = {
-        ["<Tab>"] = cmp.mapping.confirm({ select = true }),
-        ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-        ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-d>'] = cmp.mapping.scroll_docs(4),
-      },
-      matching = {
-        disallow_fuzzy_matching = false,
-        disallow_fullfuzzy_matching = false,
-        disallow_partial_fuzzy_matching = false,
-        disallow_partial_matching = false,
-        disallow_prefix_unmatching = false,
-      },
-      snippet = {
-        expand = function(args)
-          require("luasnip").lsp_expand(args.body)
-        end,
-      },
-      -- source: https://github.com/gennaro-tedesco/dotfiles/blob/4a175cce9f8f445543ac61cc6c4d6a95d6a6da10/nvim/lua/plugins/cmp.lua#L79-L88
-      -- needs testing
-      sorting = {
-        comparators = {
-          compare.score,
-          compare.offset,
-          compare.recently_used,
-          compare.order,
-          compare.exact,
-          compare.kind,
-          compare.locality,
-          compare.length,
-          -- copied from TJ Devries; cmp-under
-          function(entry1, entry2)
-            local _, entry1_under = entry1.completion_item.label:find "^_+"
-            local _, entry2_under = entry2.completion_item.label:find "^_+"
-            entry1_under = entry1_under or 0
-            entry2_under = entry2_under or 0
-            if entry1_under > entry2_under then
-              return false
-            elseif entry1_under < entry2_under then
-              return true
-            end
-          end,
-        },
-      },
-      sources = {
-        { name = "luasnip" },
-        { name = "nvim_lsp", max_item_count = 20 },
-        { name = "nvim_lua", max_item_count = 20 },
-        { name = "buffer",   max_item_count = 5 },
-        { name = 'orgmode' },
-        { name = "path" },
-        { name = 'emoji' },
-        { name = "git" },
-        {
-          name = "latex_symbols",
-          max_item_count = 10,
-          option = {
-            strategy = 0, -- mixed
-          },
-        },
-      },
-      view = {
-        entries = {
-          follow_cursor = true,
-        }
-      },
-      window = {
-        completion = {
-          border = tools.ui.borders.none,
-          col_offset = 1,
-          scrolloff = 10,
-          side_padding = pad_len,
-        },
-        documentation = {
-          border = tools.ui.borders.cur_border,
-        }
-      }
-    })
-
-    cmp.setup.cmdline(':', {
-      mapping = {
-        ["<Tab>"] = {
-          c = cmp.mapping.confirm({ select = true })
-        },
-        ['<C-n>'] = {
-          c = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select })
-        },
-        ['<C-p>'] = {
-          c = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select })
-        }
-      },
-      sources = cmp.config.sources({
-        {
-          name = 'cmdline',
-          option = {
-            treat_trailing_slash = false
-          }
-        },
-        { name = 'path', max_item_count = 20, },
-      }),
-    })
-
-    for k, v in pairs({
-      CmpItemAbbrDeprecated = { strikethrough = true },
-    }) do
-      vim.api.nvim_set_hl(0, k, v)
-    end
-
-    for k, v in pairs({
-      CmpItemAbbrMatch = "PmenuMatch",
-      CmpItemKindVariable = "Directory",
-      CmpItemKindFunction = "Constant",
-      CmpItemKindKeyword = "StatusLine",
-      CmpItemKindClass = "SpecialComment",
-      CmpItemAbbrMatchFuzzy = "CmpItemAbbrMatch",
-
-      CmpItemKindProperty = "CmpItemKindVariable",
-      CmpItemKindField = "CmpItemKindVariable",
-      CmpItemKindInterface = "CmpItemKindVariable",
-      CmpItemKindOperator = "CmpItemKindVariable",
-
-      CmpItemKindText = "CmpItemKindKeyword",
-
-      CmpItemKindMethod = "CmpItemKindFunction",
-      CmpItemKindConstant = "CmpItemKindFunction",
-      CmpItemKindValue = "CmpItemKindFunction",
-      CmpItemKindEnum = "CmpItemKindFunction",
-      CmpItemKindEnumMember = "CmpItemKindFunction",
-
-      CmpItemKindUnit = "CmpItemKindKeyword",
-      CmpItemKindSnippet = "CmpItemKindKeyword",
-
-      CmpItemKindModule = "CmpItemKindClass",
-      CmpItemKindStruct = "CmpItemKindClass",
-    }) do
-      vim.api.nvim_set_hl(0, k, { link = v })
-    end
-
-    -- Use autopairs for adding parenthesis to function completions
-    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-
-    cmp.event:on(
-      'confirm_done',
-      cmp_autopairs.on_confirm_done()
-    )
-  end,
+  -- 'iguanacucumber/magazine.nvim',
+  -- event = { 'CmdlineEnter', 'InsertEnter' },
+  -- dependencies = {
+  --   "hrsh7th/cmp-buffer",
+  --   "hrsh7th/cmp-cmdline",
+  --   "hrsh7th/cmp-emoji",
+  --   "kdheepak/cmp-latex-symbols",
+  --   "hrsh7th/cmp-nvim-lsp",
+  --   "hrsh7th/cmp-nvim-lua",
+  --   "hrsh7th/cmp-path",
+  --   "L3MON4D3/LuaSnip",
+  --   "saadparwaiz1/cmp_luasnip",
+  --   "windwp/nvim-autopairs",
+  -- },
+  -- config = function()
+  --   local cmp = require("cmp")
+  --   local compare = cmp.config.compare
+  --   local pad_len = 1
+  --
+  --   --[[
+  --         Get completion context, such as namespace where item is from.
+  --         Depending on the LSP, this information is stored in different places.
+  --         The process to find them is very manual: log the payloads And see where useful information is stored.
+  --
+  --         See https://www.reddit.com/r/neovim/comments/128ndxk/comment/jen9444/?utm_source=share&utm_medium=web2x&context=3
+  --     ]]
+  --   local function get_lsp_completion_context(completion, source)
+  --     local ok, source_name = pcall(function()
+  --       return source.source.client.config.name
+  --     end)
+  --
+  --     if not ok then
+  --       return nil
+  --     end
+  --
+  --     if source_name == "ts_ls" then
+  --       return completion.detail
+  --     elseif source_name == "pyright" and completion.labelDetails ~= nil then
+  --       return completion.labelDetails.description
+  --     elseif source_name == "texlab" then
+  --       return completion.detail
+  --     elseif source_name == "clangd" then
+  --       local doc = completion.documentation
+  --       if doc == nil then return end
+  --
+  --       local import_str = doc.value
+  --
+  --       local i, j = string.find(import_str, "[\"<].*[\">]")
+  --       if i == nil then return end
+  --
+  --       return string.sub(import_str, i, j)
+  --     end
+  --
+  --     return nil
+  --   end
+  --
+  --   cmp.setup({
+  --     formatting = {
+  --       fields = { "kind", "abbr", "menu" },
+  --       format = function(entry, vim_item)
+  --         if not require("cmp.utils.api").is_cmdline_mode() then
+  --           local abbr_width_max = 25
+  --           local menu_width_max = 20
+  --
+  --           local choice = require("lspkind").cmp_format({
+  --             ellipsis_char = tools.ui.icons.ellipses,
+  --             maxwidth = abbr_width_max + menu_width_max,
+  --             mode = "symbol",
+  --           })(entry, vim_item)
+  --
+  --           choice.abbr = vim.trim(choice.abbr)
+  --
+  --           local cmp_ctx = get_lsp_completion_context(entry.completion_item, entry.source)
+  --           if cmp_ctx ~= nil and cmp_ctx ~= "" then
+  --             choice.menu = cmp_ctx
+  --             local menu_width = string.len(choice.menu)
+  --
+  --             if menu_width > menu_width_max then
+  --               choice.menu = vim.fn.strcharpart(choice.menu, 0, menu_width_max - 1)
+  --               choice.menu = choice.menu .. tools.ui.icons.ellipses
+  --             else
+  --               local padding = string.rep(' ', menu_width_max - menu_width)
+  --               choice.menu = padding .. choice.menu
+  --             end
+  --           else
+  --             choice.menu = ""
+  --             abbr_width_max = abbr_width_max + (menu_width_max / 2)
+  --           end
+  --
+  --           -- give padding until min/max width is met
+  --           -- https://github.com/hrsh7th/nvim-cmp/issues/980#issuecomment-1121773499
+  --           local abbr_width = string.len(choice.abbr)
+  --           if abbr_width < abbr_width_max then
+  --             local padding = string.rep(' ', abbr_width_max - abbr_width)
+  --             vim_item.abbr = choice.abbr .. padding
+  --           end
+  --
+  --           return choice
+  --         else
+  --           local abbr_width_min = 12
+  --           local abbr_width_max = 50
+  --
+  --           local choice = require("lspkind").cmp_format({
+  --             ellipsis_char = tools.ui.icons.ellipses,
+  --             maxwidth = abbr_width_max,
+  --             mode = "symbol",
+  --           })(entry, vim_item)
+  --
+  --           choice.abbr = vim.trim(choice.abbr)
+  --
+  --           -- give padding until min/max width is met
+  --           -- https://github.com/hrsh7th/nvim-cmp/issues/980#issuecomment-1121773499
+  --           local abbr_width = string.len(choice.abbr)
+  --           if abbr_width < abbr_width_min then
+  --             local padding = string.rep(' ', abbr_width_min - abbr_width)
+  --             vim_item.abbr = choice.abbr .. padding
+  --           end
+  --
+  --           return choice
+  --         end
+  --       end,
+  --     },
+  --     mapping = {
+  --       ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+  --       ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+  --       ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+  --       ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+  --       ['<C-d>'] = cmp.mapping.scroll_docs(4),
+  --     },
+  --     matching = {
+  --       disallow_fuzzy_matching = false,
+  --       disallow_fullfuzzy_matching = false,
+  --       disallow_partial_fuzzy_matching = false,
+  --       disallow_partial_matching = false,
+  --       disallow_prefix_unmatching = false,
+  --     },
+  --     snippet = {
+  --       expand = function(args)
+  --         require("luasnip").lsp_expand(args.body)
+  --       end,
+  --     },
+  --     -- source: https://github.com/gennaro-tedesco/dotfiles/blob/4a175cce9f8f445543ac61cc6c4d6a95d6a6da10/nvim/lua/plugins/cmp.lua#L79-L88
+  --     -- needs testing
+  --     sorting = {
+  --       comparators = {
+  --         compare.score,
+  --         compare.offset,
+  --         compare.recently_used,
+  --         compare.order,
+  --         compare.exact,
+  --         compare.kind,
+  --         compare.locality,
+  --         compare.length,
+  --         -- copied from TJ Devries; cmp-under
+  --         function(entry1, entry2)
+  --           local _, entry1_under = entry1.completion_item.label:find "^_+"
+  --           local _, entry2_under = entry2.completion_item.label:find "^_+"
+  --           entry1_under = entry1_under or 0
+  --           entry2_under = entry2_under or 0
+  --           if entry1_under > entry2_under then
+  --             return false
+  --           elseif entry1_under < entry2_under then
+  --             return true
+  --           end
+  --         end,
+  --       },
+  --     },
+  --     sources = {
+  --       { name = "luasnip" },
+  --       { name = "nvim_lsp", max_item_count = 20 },
+  --       { name = "nvim_lua", max_item_count = 20 },
+  --       { name = "buffer",   max_item_count = 5 },
+  --       { name = 'orgmode' },
+  --       { name = "path" },
+  --       { name = 'emoji' },
+  --       { name = "git" },
+  --       {
+  --         name = "latex_symbols",
+  --         max_item_count = 10,
+  --         option = {
+  --           strategy = 0, -- mixed
+  --         },
+  --       },
+  --     },
+  --     view = {
+  --       entries = {
+  --         follow_cursor = true,
+  --       }
+  --     },
+  --     window = {
+  --       completion = {
+  --         border = tools.ui.borders.none,
+  --         col_offset = 1,
+  --         scrolloff = 10,
+  --         side_padding = pad_len,
+  --       },
+  --       documentation = {
+  --         border = tools.ui.borders.cur_border,
+  --       }
+  --     }
+  --   })
+  --
+  --   cmp.setup.cmdline(':', {
+  --     mapping = {
+  --       ["<Tab>"] = {
+  --         c = cmp.mapping.confirm({ select = true })
+  --       },
+  --       ['<C-n>'] = {
+  --         c = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select })
+  --       },
+  --       ['<C-p>'] = {
+  --         c = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+  --       }
+  --     },
+  --     sources = cmp.config.sources({
+  --       {
+  --         name = 'cmdline',
+  --         option = {
+  --           treat_trailing_slash = false
+  --         }
+  --       },
+  --       { name = 'path', max_item_count = 20, },
+  --     }),
+  --   })
+  --
+  --   -- Use autopairs for adding parenthesis to function completions
+  --   local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+  --
+  --   cmp.event:on(
+  --     'confirm_done',
+  --     cmp_autopairs.on_confirm_done()
+  --   )
+  -- end,
 }
