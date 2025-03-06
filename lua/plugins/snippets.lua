363709912ca76a77b59b3d73e1d24004f900e46d
McAuley Penney <jacobmpenney@gmail.com>
2025-03-05 19:34:09 -0700

    refactor: update mini.snippets settings

--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -1,25 +1,25 @@
 return {
   "echasnovski/mini.snippets",
-  init = function()
-    local snips = require('mini.snippets')
-    local gen_loader = snips.gen_loader
+  opts = {
+    snippets = {
+      --  gen_loader.from_file('~/.config/nvim/snippets/global.json'),
+      require('mini.snippets').gen_loader.from_lang(),
+    },
+    mappings = {
+      stop = '<C-c>'
+    },
+    expand = {
+      insert = function(snippet)
+        return require('mini.snippets').default_insert(snippet, {
+          empty_tabstop = '',
+          empty_tabstop_final = '†'
+        })
+      end
+    }
+  },
+  config = function(_, opts)
+    require('mini.snippets').setup(opts)
 
-    snips.setup({
-      snippets = {
-        --  gen_loader.from_file('~/.config/nvim/snippets/global.json'),
-        gen_loader.from_lang(),
-      },
-      expand = {
-        insert = function(snippet)
-          return snips.default_insert(snippet, { empty_tabstop = '○', empty_tabstop_final = '†' })
-        end
-      }
-    })
-
-    vim.api.nvim_set_hl(0, 'MiniSnippetsCurrent', { link = "NONE" })
-    vim.api.nvim_set_hl(0, 'MiniSnippetsCurrentReplace', { link = "Operator" })
     vim.api.nvim_set_hl(0, 'MiniSnippetsFinal', { link = "Comment" })
-    vim.api.nvim_set_hl(0, 'MiniSnippetsUnvisited', { link = "Comment" })
-    vim.api.nvim_set_hl(0, 'MiniSnippetsVisited', { link = "NONE" })
-  end
+  end,
 }
