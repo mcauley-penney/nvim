a730ba3454fa773c7f8c7e786b176680ee3f9017
McAuley Penney <jacobmpenney@gmail.com>
2025-05-02 14:16:30 -0700

    feat(plugins/dropbar): allow dropbar to work in text buffers

--- lua/plugins/dropbar.lua
+++ lua/plugins/dropbar.lua
@@ -2,23 +2,47 @@ return {
   "Bekaboo/dropbar.nvim",
   opts = {
     bar = {
+      enable = function(buf, win, _)
+        if
+          not vim.api.nvim_buf_is_valid(buf)
+          or not vim.api.nvim_win_is_valid(win)
+          or vim.fn.win_gettype(win) ~= ""
+          or vim.wo[win].winbar ~= ""
+          or vim.bo[buf].ft == "help"
+        then
+          return false
+        end
+
+        local stat = vim.uv.fs_stat(vim.api.nvim_buf_get_name(buf))
+        if stat and stat.size > 1024 * 1024 then return false end
+
+        return vim.bo[buf].ft == "markdown"
+          or vim.bo[buf].ft == "text"
+          or pcall(vim.treesitter.get_parser, buf)
+          or not vim.tbl_isempty(vim.lsp.get_clients({
+            bufnr = buf,
+            method = "textDocument/documentSymbol",
+          }))
+      end,
       update_debounce = 100,
       sources = function(buf, _)
-        local sources = require('dropbar.sources')
-        local utils = require('dropbar.utils')
+        local sources = require("dropbar.sources")
+        local utils = require("dropbar.utils")
 
-        if vim.bo[buf].ft == 'markdown' then return { sources.markdown } end
-        if vim.bo[buf].buftype == 'terminal' then return { sources.terminal } end
+        if vim.bo[buf].ft == "markdown" then return { sources.markdown } end
+        if vim.bo[buf].buftype == "terminal" then
+          return { sources.terminal }
+        end
         return {
           utils.source.fallback({
             sources.lsp,
             sources.treesitter,
           }),
         }
-      end
+      end,
     },
     icons = {
-      ui = { bar = { separator = ' ' .. tools.ui.icons.r_chev .. ' ' } },
-    }
+      ui = { bar = { separator = " " .. tools.ui.icons.r_chev .. " " } },
+    },
   },
 }
