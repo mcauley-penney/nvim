171ee0988b98cdd5d34db9467074cf037d81973b
McAuley Penney <jacobmpenney@gmail.com>
2025-05-10 13:26:15 -0700

    refactor(cursorword): make delay shorter

--- lua/plugins/illuminate.lua
+++ lua/plugins/illuminate.lua
@@ -3,7 +3,7 @@ return {
     "echasnovski/mini.cursorword",
     version = false,
     config = function()
-      require("mini.cursorword").setup({ delay = 500 })
+      require("mini.cursorword").setup({ delay = 350 })
 
       vim.api.nvim_set_hl(0, "MiniCursorwordCurrent", {})
       vim.api.nvim_set_hl(0, "MiniCursorword", { link = "Underlined" })
