f2440428eb470f4ea62f874a8f114b9c42423958
McAuley Penney <jacobmpenney@gmail.com>
2025-05-11 10:35:51 -0700

    refactor(illuminate): move back to illuminate
    - I prefer vim-illuminate's functionality and settings
    - rm minimum length for match-visual
    - move match-visual highlight to colorscheme

--- lua/plugins/illuminate.lua
+++ lua/plugins/illuminate.lua
@@ -1,24 +1,21 @@
 return {
   {
-    "echasnovski/mini.cursorword",
-    version = false,
+    "rrethy/vim-illuminate",
     config = function()
-      require("mini.cursorword").setup({ delay = 350 })
-
-      vim.api.nvim_set_hl(0, "MiniCursorwordCurrent", {})
-      vim.api.nvim_set_hl(0, "MiniCursorword", { link = "Underlined" })
+      require("illuminate").configure({
+        delay = 350,
+        filetypes_denylist = {
+          "aerial",
+          "neo-tree",
+        },
+        modes_denylist = { "v", "V" },
+        under_cursor = false,
+      })
     end,
   },
 
   {
     "mcauley-penney/match-visual.nvim",
-    opts = {
-      min_length = 3,
-    },
-    config = function(_, opts)
-      require("match-visual").setup(opts)
-      vim.api.nvim_set_hl(0, "VisualMatch", { link = "Search" })
-    end,
   },
 
   {
