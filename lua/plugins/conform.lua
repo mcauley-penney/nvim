86c755d3d450137593b6c50e6aa4cb61eef4744c
McAuley Penney <jacobmpenney@gmail.com>
2025-05-12 16:13:37 -0700

    refactor(conform): minor changes
    - add black for Python
    - add tex-fmt with custom settings
    - reorganize settings
    - format with Stylua

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,17 +1,27 @@
 return {
-  'stevearc/conform.nvim',
-  --  event = 'BufReadPre',
+  "stevearc/conform.nvim",
   opts = {
-    formatters_by_ft = {
-      bib = { 'bibtex-tidy' },
-      lua = { 'stylua' },
-      markdown = { 'prettier' }
-    },
     default_format_opts = {
       lsp_format = "fallback",
     },
+    formatters_by_ft = {
+      bib = { "bibtex-tidy" },
+      html = { "prettier" },
+      lua = { "stylua" },
+      markdown = { "prettier" },
+      python = { "black" },
+      tex = { "tex-fmt" },
+    },
+    formatters = {
+      ["tex-fmt"] = {
+        prepend_args = { "--nowrap", "--tabsize", "4" },
+      },
+    },
   },
-  vim.keymap.set('n', "<leader>f", function()
-    require("conform").format()
-  end, { desc = "[f]ormat with LSP" })
+  vim.keymap.set(
+    "n",
+    "<leader>f",
+    function() require("conform").format() end,
+    { desc = "[f]ormat with LSP" }
+  ),
 }
