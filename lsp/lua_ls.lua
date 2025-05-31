72ac35a490653ba91c2afa8f07d01c34463cb967
McAuley Penney <jacobmpenney@gmail.com>
2025-05-31 16:29:47 -0700

    refactor(lsp): remove unnecessary lua_ls opts

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -1,13 +1,6 @@
 -- https://github.com/LuaLS/lua-language-server/blob/f7e0e7a4245578af8cef9eb5e3ec9ce65113684e/locale/en-us/setting.lua
 
 return {
-  cmd = { "lua-language-server" },
-  filetypes = { "lua" },
-  root_markers = { ".luarc.json", ".luarc.jsonc" },
-  telemetry = { enabled = false },
-  formatters = {
-    ignoreComments = false,
-  },
   settings = {
     Lua = {
       runtime = {
