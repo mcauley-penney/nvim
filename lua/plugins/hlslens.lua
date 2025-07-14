43d2017337648cd4fb8b44793f3c584fd1f2c032
McAuley Penney <jacobmpenney@gmail.com>
2025-07-14 07:17:17 -0700

    feat(hlslens): make virt text more informative and prettier

--- lua/plugins/hlslens.lua
+++ lua/plugins/hlslens.lua
@@ -2,34 +2,51 @@ local function lens(render, pos_list, nearest, wkg_i, _)
   local hl = "Folded"
   local chunks
 
-  local cur_ratio = (' %d/%d '):format(wkg_i, #pos_list)
-  chunks = { { '  ', 'Ignore' }, { cur_ratio, hl } }
+  local pattern = vim.fn.getreg("/")
+  pattern = pattern:gsub("^\\<", ""):gsub("\\>$", "")
+
+  local cur_ratio = "(" .. ("%d/%d"):format(wkg_i, #pos_list) .. ")"
+  chunks = {
+    { "   ", "Ignore" },
+    { [[ ⌕ "]], hl },
+    { pattern, hl },
+    { [[" ]], hl },
+    { cur_ratio, hl },
+    { " ", hl },
+  }
 
   local lnum, col = pos_list[wkg_i][1], pos_list[wkg_i][2]
   render.setVirt(0, lnum - 1, col - 1, chunks, nearest)
 end
 
 return {
-  'kevinhwang91/nvim-hlslens',
+  "kevinhwang91/nvim-hlslens",
   opts = {
     calm_down = true,
-    override_lens = lens
+    enable_incsearch = false,
+    override_lens = lens,
   },
   init = function()
     local map = vim.api.nvim_set_keymap
     local kopts = { noremap = true, silent = true }
 
-    map('n', 'j',
+    map(
+      "n",
+      "j",
       [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]],
-      kopts)
-    map('n', 'J',
+      kopts
+    )
+    map(
+      "n",
+      "J",
       [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]],
-      kopts)
-    map('n', '*', [[*<Cmd>lua require('hlslens').start()<CR>]], kopts)
-    map('n', '#', [[#<Cmd>lua require('hlslens').start()<CR>]], kopts)
-    map('n', 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>]], kopts)
-    map('n', 'g#', [[g#<Cmd>lua require('hlslens').start()<CR>]], kopts)
+      kopts
+    )
+    map("n", "*", [[*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+    map("n", "#", [[#<Cmd>lua require('hlslens').start()<CR>]], kopts)
+    map("n", "g*", [[g*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+    map("n", "g#", [[g#<Cmd>lua require('hlslens').start()<CR>]], kopts)
 
-    map('n', '<Leader>l', '<Cmd>noh<CR>', kopts)
-  end
+    map("n", "<Leader>l", "<Cmd>noh<CR>", kopts)
+  end,
 }
