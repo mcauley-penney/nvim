777d18b5a97b26d75f0f00c83f25d63bf3250470
McAuley Penney <jacobmpenney@gmail.com>
2025-09-19 19:35:17 -0700

    refactor(color schemes): I forgot to officially move to my own color scheme

--- lua/plugins/schemes.lua
+++ lua/plugins/schemes.lua
@@ -1,17 +1,5 @@
 return {
-  --  {
-  --    dir = "/home/m/files/projects/colorschemes/ice-cave.nvim/",
-  --    priority = 1000,
-  --    init = function()
-  --      vim.cmd.colorscheme("ice-cave")
-  --    end,
-  --  },
-
-  {
-    "mcauley-penney/phobos-anomaly.nvim",
-    priority = 1000,
-    init = function()
-      vim.cmd.colorscheme("phobos-anomaly")
-    end,
-  },
+  "mcauley-penney/techbase.nvim",
+  priority = 1000,
+  init = function() vim.cmd.colorscheme("techbase") end,
 }
