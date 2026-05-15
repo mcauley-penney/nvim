bbb325c9099eaa9b3526e07fb009d22f3bf4d98d
McAuley Penney <jacobmpenney@gmail.com>
2026-05-14 17:22:52 -0700

    refactor: remove old aucmd that imitated scrolloffpad

--- lua/aucmd/init.lua
+++ lua/aucmd/init.lua
@@ -77,23 +77,6 @@ aucmd("LspAttach", {
 ---- during editing
 grp = augrp("Editing", { clear = true })
 
-vim.api.nvim_create_autocmd({ "BufEnter", "CursorMoved", "CursorHoldI" }, {
-  group = grp,
-  callback = function()
-    local win_h = vim.api.nvim_win_get_height(0)
-    local off = math.min(vim.o.scrolloff, math.floor(win_h / 2))
-    local dist = vim.fn.line("$") - vim.fn.line(".")
-    local rem = vim.fn.line("w$") - vim.fn.line("w0") + 1
-
-    if dist < off and win_h - rem + dist < off then
-      local view = vim.fn.winsaveview()
-      view.topline = view.topline + off - (win_h - rem + dist)
-      vim.fn.winrestview(view)
-    end
-  end,
-  desc = "When at eob, bring the current line towards center screen",
-})
-
 vim.api.nvim_create_autocmd("VimResized", {
   group = grp,
   command = [[tabdo wincmd =]],
